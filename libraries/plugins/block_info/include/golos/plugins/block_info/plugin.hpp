2fb2d446668bc678ac0c93a3003946f8adc00a8c
Anton <antokorp@gmail.com>
2017-11-09 23:20:16 +0300

    Changed block_info plugin structure for saving encapsulation pattern. #159

--- libraries/plugins/block_info/include/golos/plugins/block_info/plugin.hpp
+++ libraries/plugins/block_info/include/golos/plugins/block_info/plugin.hpp
@@ -2,56 +2,98 @@
 
 #include <appbase/application.hpp>
 #include <golos/plugins/block_info/block_info.hpp>
+
 #include <boost/program_options.hpp>
 #include <string>
 #include <vector>
 #include <golos/plugins/chain/plugin.hpp>
+#include <golos/plugins/json_rpc/utility.hpp>
+#include <golos/plugins/json_rpc/plugin.hpp>
 
 namespace golos {
-    namespace protocol {
-        struct signed_block;
-    }
+namespace protocol {
+    struct signed_block;
+}
 }
 
 
 namespace golos {
-    namespace plugins {
-        namespace block_info {
-            using golos::plugins::block_info_api::api;
-            using boost::program_options::options_description;
+namespace plugins {
+namespace block_info {
 
-            class plugin final : public appbase::plugin<plugin> {
-            public:
-                APPBASE_PLUGIN_REQUIRES((golos::plugins::chain::plugin))
+    using golos::plugins::json_rpc::msg_pack;
 
-                constexpr const static char *plugin_name = "block_info";
+    struct get_block_info_a {
+        uint32_t start_block_num = 0;
+        uint32_t count = 1000;
+    };
 
-                static const std::string &name() {
-                    static std::string name = plugin_name;
-                    return name;
-                }
+    struct get_blocks_with_info_a {
+        uint32_t start_block_num = 0;
+        uint32_t count = 1000;
+    };    
 
-                plugin();
+    struct get_block_info_r {
+        std::vector<block_info> block_info_vec;
+    };
 
-                ~plugin();
+    struct get_blocks_with_info_r {
+        std::vector<block_with_info> block_with_info_vec;
+    };
 
-                void set_program_options(boost::program_options::options_description &cli, boost::program_options::options_description &cfg) override {
-                }
+    DEFINE_API_ARGS ( get_block_info,           msg_pack,       get_block_info_r       )
+    DEFINE_API_ARGS ( get_blocks_with_info,     msg_pack,       get_blocks_with_info_r )
 
-                void plugin_initialize(const boost::program_options::variables_map &options) override;
 
-                void plugin_startup() override;
+    using boost::program_options::options_description;
 
-                void plugin_shutdown() override;
+    class plugin final : public appbase::plugin<plugin> {
+    public:
+        APPBASE_PLUGIN_REQUIRES((golos::plugins::chain::plugin))
 
-                void on_applied_block(const protocol::signed_block &b);
+        constexpr const static char *plugin_name = "block_info";
 
-                std::vector<block_info> _block_info;
-
-                boost::signals2::scoped_connection _applied_block_conn;
-
-                std::shared_ptr<api> api_ptr;
-            };
+        static const std::string &name() {
+            static std::string name = plugin_name;
+            return name;
         }
-    }
-}
+
+        plugin();
+
+        ~plugin();
+
+        void set_program_options(boost::program_options::options_description &cli, boost::program_options::options_description &cfg) override {
+        }
+
+        void plugin_initialize(const boost::program_options::variables_map &options) override;
+
+        void plugin_startup() override;
+
+        void plugin_shutdown() override;
+
+        DECLARE_API(    (get_block_info)        )
+        DECLARE_API(    (get_blocks_with_info)  )
+        void on_applied_block(const protocol::signed_block &b);
+
+    private:
+        class impl;
+
+        std::shared_ptr<impl> my;
+    };
+} } }
+
+FC_REFLECT((golos::plugins::block_info::get_block_info_a),
+        (start_block_num)(count)
+)
+
+FC_REFLECT((golos::plugins::block_info::get_blocks_with_info_a),
+        (start_block_num)(count)
+)
+
+FC_REFLECT((golos::plugins::block_info::get_block_info_r),
+        (block_info_vec)
+)
+
+FC_REFLECT((golos::plugins::block_info::get_blocks_with_info_r),
+        (block_with_info_vec)
+)
--- libraries/plugins/block_info/plugin.cpp
+++ libraries/plugins/block_info/plugin.cpp
@@ -1,58 +1,160 @@
 #include <golos/chain/database.hpp>
 
 #include <golos/plugins/block_info/block_info.hpp>
-#include <golos/plugins/block_info/api.hpp>
+// #include <golos/plugins/block_info/api.hpp>
 #include <golos/plugins/block_info/plugin.hpp>
 
+#include <golos/protocol/types.hpp>
+#include <golos/plugins/json_rpc/utility.hpp>
+#include <golos/plugins/json_rpc/plugin.hpp>
+
 namespace golos {
-    namespace plugins {
-        namespace block_info {
-            using golos::plugins::block_info_api;
-            using namespace golos::chain;
+namespace plugins {
+namespace block_info {
+    using namespace golos::chain;
 
-            plugin::plugin() {
-            }
-
-            plugin::~plugin() {
-            }
-
-            void plugin::plugin_initialize(const boost::program_options::variables_map &options) {
-                auto &db = appbase::app().get_plugin<chain::plugin>().db();
-
-                _applied_block_conn = db.applied_block.connect([this](const protocol::signed_block &b) {
-                    on_applied_block(b);
-                });
-
-                api_ptr = std::make_shared<api>(_block_info);
-            }
-
-            void plugin::plugin_startup() {
-            }
-
-            void plugin::plugin_shutdown() {
-            }
-
-            void plugin::on_applied_block(const protocol::signed_block &b) {
-                uint32_t block_num = b.block_num();
-                const auto &db = appbase::app().get_plugin<chain::plugin>().db();
-
-                while (block_num >= _block_info.size()) {
-                    _block_info.emplace_back();
-                }
-
-                block_info &info = _block_info[block_num];
-                const dynamic_global_property_object &dgpo = db.get_dynamic_global_properties();
-
-                info.block_id = b.id();
-                info.block_size = fc::raw::pack_size(b);
-                info.average_block_size = dgpo.average_block_size;
-                info.aslot = dgpo.current_aslot;
-                info.last_irreversible_block_num = dgpo.last_irreversible_block_num;
-                info.num_pow_witnesses = dgpo.num_pow_witnesses;
-                return;
-            }
-
+    class plugin::impl  {
+    public:
+        impl() : db_(appbase::app().get_plugin<plugins::chain::plugin>().db()) {
         }
+
+        // DECLARE_API(    (get_block_info)        )
+        // DECLARE_API(    (get_blocks_with_info)  )
+        // API
+        get_block_info_r get_block_info(const get_block_info_a & args);
+        get_blocks_with_info_r get_blocks_with_info(const get_blocks_with_info_a & args);
+
+        // PLUGIN_METHODS
+        void on_applied_block(const protocol::signed_block &b);
+
+        // HELPING METHODS
+        golos::chain::database &database() {
+            return db_;
+        }
+    // protected:
+        boost::signals2::scoped_connection applied_block_conn_;
+    private:
+        std::vector<block_info> block_info_;
+
+        golos::chain::database & db_;
+    };
+
+    get_block_info_r plugin::impl::get_block_info(const get_block_info_a & args) {
+        get_block_info_r result;
+    
+        FC_ASSERT(args.start_block_num > 0);
+        FC_ASSERT(args.count <= 10000);
+        uint32_t n = std::min(uint32_t(block_info_.size()),
+        args.start_block_num + args.count);
+
+        for (uint32_t block_num = args.start_block_num;
+            block_num < n; block_num++) {
+            result.block_info_vec.emplace_back(block_info_[block_num]);
+        }
+
+        return result;
     }
+
+    get_blocks_with_info_r plugin::impl::get_blocks_with_info(const get_blocks_with_info_a & args) {    
+        get_blocks_with_info_r result;
+        const auto & db = database();
+
+        FC_ASSERT(args.start_block_num > 0);
+        FC_ASSERT(args.count <= 10000);
+        uint32_t n = std::min( uint32_t( block_info_.size() ), args.start_block_num + args.count );
+
+        uint64_t total_size = 0;
+        for (uint32_t block_num = args.start_block_num;
+             block_num < n; block_num++) {
+            uint64_t new_size =
+                    total_size + block_info_[block_num].block_size;
+            if ((new_size > 8 * 1024 * 1024) &&
+                (block_num != args.start_block_num)) {
+                    break;
+            }
+            total_size = new_size;
+            result.block_with_info_vec.emplace_back();
+            result.block_with_info_vec.back().block = *db.fetch_block_by_number(block_num);
+            result.block_with_info_vec.back().info = block_info_[block_num];
+        }
+
+        return result;
+    }
+
+    void plugin::impl::on_applied_block(const protocol::signed_block &b) {
+        uint32_t block_num = b.block_num();
+        const auto &db = appbase::app().get_plugin<chain::plugin>().db();
+
+        while (block_num >= block_info_.size()) {
+            block_info_.emplace_back();
+        }
+
+        block_info &info = block_info_[block_num];
+        const dynamic_global_property_object &dgpo = db.get_dynamic_global_properties();
+
+        info.block_id = b.id();
+        info.block_size = fc::raw::pack_size(b);
+        info.average_block_size = dgpo.average_block_size;
+        info.aslot = dgpo.current_aslot;
+        info.last_irreversible_block_num = dgpo.last_irreversible_block_num;
+        info.num_pow_witnesses = dgpo.num_pow_witnesses;
+        return;
+    }
+
+
+    DEFINE_API ( plugin, get_block_info ) {
+        auto tmp = args.args->at(0).as<get_block_info_a>();
+        auto &db = my->database();
+        return db.with_read_lock([&]() {
+            get_block_info_r result;
+            result = my->get_block_info(tmp);
+            return result;
+        });
+    }
+
+    DEFINE_API ( plugin, get_blocks_with_info ) {
+        auto tmp = args.args->at(0).as<get_blocks_with_info_a>();
+        auto &db = my->database();
+        return db.with_read_lock([&]() {
+            get_blocks_with_info_r result;
+            result = my->get_blocks_with_info(tmp);
+            return result;
+        });
+    }
+
+     void plugin::on_applied_block(const protocol::signed_block &b) {
+        return my->on_applied_block(b);
+     }
+
+
+    plugin::plugin() {
+    }
+
+    plugin::~plugin() {
+    }
+
+    void plugin::plugin_initialize(const boost::program_options::variables_map &options) {
+
+        auto &db = appbase::app().get_plugin<chain::plugin>().db();
+
+
+        my = std::make_shared<impl>();
+        my->applied_block_conn_ = db.applied_block.connect([this](const protocol::signed_block &b) {
+            on_applied_block(b);
+        });
+
+        JSON_RPC_REGISTER_API ( name() ) ;
+    }
+
+    void plugin::plugin_startup() {
+    }
+
+    void plugin::plugin_shutdown() {
+    }
+
+
+}
+}
 } // golos::plugin::block_info
+
 
