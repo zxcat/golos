95cd4c6eae844066c1a8873cba9dab6b30a772cf
Semen Medvedev <kiathai@gmail.com>
2018-07-20 15:29:44 +0700

    Refactor errors: fix common macros #790

--- tests/common/helpers.hpp
+++ tests/common/helpers.hpp
@@ -15,26 +15,16 @@
 
 /// change operation parameter value and check it's invalid (+restore param)
 #define CHECK_PARAM_INVALID(OP, N, V) \
-    CHECK_PARAM_VALIDATION_FAIL(OP, N, V, invalid_parameter, #N)
+    CHECK_PARAM_VALIDATION_FAIL(OP, N, V, CHECK_ERROR(invalid_parameter, #N))
 
 /// change operation parameter value and check it fails with logic_exceprion (+restore param)
 #define CHECK_PARAM_INVALID_LOGIC(OP, N, V, EX) \
-    CHECK_PARAM_VALIDATION_FAIL(OP, N, V, logic_exception, logic_exception:: EX)
+    CHECK_PARAM_VALIDATION_FAIL(OP, N, V, CHECK_ERROR(logic_exception, logic_exception:: EX))
 
 /// same as previous but with configurable CHECK_ERROR parameters
-#define CHECK_PARAM_VALIDATION_FAIL(OP, N, V, EX, EV) \
+#define CHECK_PARAM_VALIDATION_FAIL(OP, N, V, VALIDATOR) \
     CHECK_PARAM_VALID_I(OP, N, V, \
-        GOLOS_CHECK_ERROR_PROPS(OP.validate(), CHECK_ERROR(EX, EV)))
-
-
-// Push tx
-#define GOLOS_TEST_TX_THROW(E, KEY, SETUP) GOLOS_TEST_TX_I(SETUP, KEY, GOLOS_CHECK_PUSH_TX_THROW(E))
-#define GOLOS_TEST_TX_NO_THROW(KEY, SETUP) GOLOS_TEST_TX_I(SETUP, KEY, GOLOS_CHECK_PUSH_TX_NO_THROW)
-#define GOLOS_TEST_TX_I(SETUP, KEY, PUSH) \
-    tx.clear(); \
-    SETUP; \
-    tx.sign(KEY, db->get_chain_id()); \
-    PUSH;
+        GOLOS_CHECK_ERROR_PROPS(OP.validate(), VALIDATOR))
 
 
 // Check operation authorities
@@ -62,14 +52,6 @@
     CHECK; \
     OP.NAME = t; \
 }
-
-
-// push tx
-#define GOLOS_CHECK_PUSH_TX_NO_THROW GOLOS_CHECK_PUSH_TX_NO_THROW_F(0)
-#define GOLOS_CHECK_PUSH_TX_NO_THROW_F(F) BOOST_CHECK_NO_THROW(db->push_transaction(tx, F))
-#define GOLOS_CHECK_PUSH_TX_THROW(E) GOLOS_CHECK_PUSH_TX_THROW_F(0, E)
-#define GOLOS_CHECK_PUSH_TX_THROW_F(F, E) \
-    GOLOS_CHECK_ERROR_PROPS(db->push_transaction(tx, F), CHECK_ERROR(E, 0))
 
 
 // boost::container <<
