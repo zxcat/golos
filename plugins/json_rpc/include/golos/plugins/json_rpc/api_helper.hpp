445d7dbe123190a7456cdc82adf49114d088de3f
Semen Medvedev <kiathai@gmail.com>
2018-07-26 07:26:08 +0700

    Refactor errros: fixed PLUGIN_API_VALIDATE_ARGS to throw valid exceptions when invalid parameter #791

--- plugins/json_rpc/include/golos/plugins/json_rpc/api_helper.hpp
+++ plugins/json_rpc/include/golos/plugins/json_rpc/api_helper.hpp
@@ -1,8 +1,5 @@
 #pragma once
 
-// TODO Remove after done refactor erros in plugins #791
-//      This functional moved to plugins/jon_api/include/golos/plugins/json_api/api_helper.hpp
-
 // Provides PLUGIN_API_VALIDATE_ARGS macro usable in DEFINE_API methods
 // to generate arguments processing boilerplate.
 
@@ -12,10 +9,9 @@
 // )
 //Expands to:
 // std::string json_filename;
-// FC_ASSERT(args.args.valid(), "Invalid parameters");
 // auto n_args = args.args->size();
-// FC_ASSERT(n_args == 1, "Expected 1 parameter, received ${n}", ("n", n_args));
-// json_filename = args.args->at(0).as<std::string>();
+// GOLOS_ASSERT(n_args == 1, golos::invalid_parameter, "Expected 1 parameter, received ${n}", ("n", n_args)("required",1));
+// json_filename = GOLOS_CONVERT_PARAM(json_filename,args.args->at(0),std::string);
 //
 //And this:
 // PLUGIN_API_VALIDATE_ARGS(
@@ -27,13 +23,12 @@
 // std::string json_filename;
 // uint32_t count;
 // uint32_t skip_flags = golos::chain::database::skip_nothing;
-// FC_ASSERT(args.args.valid(), "Invalid parameters");
 // auto n_args = args.args->size();
-// FC_ASSERT(n_args >= 2 && n_args <= 3, "Expected at least 2 and up to 3 parameters, received ${n}", ("n", n_args));
-// json_filename = args.args->at(0).as<std::string>();
-// count = args.args->at(1).as<uint32_t>();
+// GOLOS_ASSERT(n_args >= 2 && n_args <= 3, golos::invalid_parameter, "Expected at least 2 and up to 3 parameters, received ${n}", ("n", n_args)("min",2)("max",3));
+// json_filename = GOLOS_CONVERT_PARAM(json_filename,args.args->at(0),std::string);
+// count = GOLOS_CONVERT_PARAM(count,args.args->at(1),uint32_t);
 // if (n_args > 2) {
-//     skip_flags = args.args->at(2).as<uint32_t>();
+//     skip_flags = GOLOS_CONVERT_PARAM(skip_flags,args.args->at(2),uint32_t);
 // }
 
 #include <boost/preprocessor/stringize.hpp>
@@ -59,7 +54,7 @@
 // main
 #define PLUGIN_API_VALIDATE_ARGS_II(ARGS, N_REQ, N_ARGS)  \
     PLUGIN_API_DECLARE_ARG_VARS(ARGS)   \
-    FC_ASSERT(args.args.valid(), "Invalid parameters"); /* is it possible to get invalid?*/     \
+    /*FC_ASSERT(args.args.valid(), "Invalid parameters"); *//* is it possible to get invalid?*/     \
     auto n_args = args.args->size();    \
     PLUGIN_API_ARGS_NUM_ASSERT(N_REQ, N_ARGS) \
     PLUGIN_API_LOAD_AND_CHECK_ARGS(ARGS, N_REQ)
@@ -90,20 +85,21 @@
 
 #define PLUGIN_API_ARGS_NUM_ASSERT(req, all) PLUGIN_API_ARGS_NUM_ASSERT_I(BOOST_PP_LESS(req, all), req, all)
 #define     PLUGIN_API_ARGS_NUM_ASSERT_I(less, req, all) \
-                PLUGIN_API_ARGS_NUM_ASSERT_II(AN_ASSERT_COND(less, req, all), AN_ASSERT_MSG(less, req, all))
-#define     PLUGIN_API_ARGS_NUM_ASSERT_II(cond, msg) \
-                FC_ASSERT(cond, msg, ("n", n_args));
+                PLUGIN_API_ARGS_NUM_ASSERT_II(AN_ASSERT_COND(less, req, all), AN_ASSERT_MSG(less, req, all), AN_ASSERT_VARS(less, req, all))
+#define     PLUGIN_API_ARGS_NUM_ASSERT_II(cond, msg, vars) \
+                GOLOS_ASSERT(cond, golos::invalid_arguments_count, msg, ("n", n_args) vars);
 
 #define     AN_ASSERT_COND(less, req, all) BOOST_PP_IF(less, n_args >= req && n_args <= all, n_args == all)
 #define     AN_ASSERT_MSG(LESS, REQ, ALL) \
                 "Expected " BOOST_PP_STRINGIZE( BOOST_PP_IF(LESS, at least REQ and up to ALL, REQ) ) \
                 " parameter" PLURAL_FINAL(ALL) ", received ${n}"
+#define     AN_ASSERT_VARS(less, req, all) BOOST_PP_IF(less, ("min",req)("max",all), ("required",all))
 #define     PLURAL_FINAL(n) BOOST_PP_IF(BOOST_PP_LESS(n, 2), "", "s")
 
 #define PLUGIN_API_LOAD_AND_CHECK_ARGS(ARGS, n_req) BOOST_PP_SEQ_FOR_EACH_I(LOAD_AND_CHECK_ARG, n_req, ARGS)
 #define     LOAD_AND_CHECK_ARG(r, n_req, i, arg) LOAD_AND_CHECK_ARG_I(arg, i, BOOST_PP_LESS(i, n_req))
 #define     LOAD_AND_CHECK_ARG_I(arg, i, req) BOOST_PP_IF(req, LOAD_ARG, LOAD_OPTIONAL_ARG)(arg, i)
-#define     LOAD_ARG(arg, i) ARG_NAME(arg) = args.args->at(i).as<ARG_TYPE(arg)>();
+#define     LOAD_ARG(arg, i) ARG_NAME(arg) = GOLOS_CONVERT_PARAM(ARG_NAME(arg), args.args->at(i), ARG_TYPE(arg));
 #define     LOAD_OPTIONAL_ARG(arg, i)   \
                 if (n_args > i) {       \
                     LOAD_ARG(arg, i)    \
