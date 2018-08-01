getflagman/flagman

Add comment_reward_object to social_network #829

libraries/api/include/golos/api/discussion.hpp
@@ -17,6 +17,8 @@ namespace golos { namespace api {
 
         string url; /// /category/@rootauthor/root_permlink#author/permlink
         asset pending_payout_value = asset(0, SBD_SYMBOL); ///< sbd
+        asset pending_payout_gests_value = asset(0, VESTS_SYMBOL);
+        asset pending_payout_golos_value = asset(0, STEEM_SYMBOL);
         asset total_pending_payout_value = asset(0, SBD_SYMBOL); ///< sbd including replies
         std::vector<vote_state> active_votes;
         uint32_t active_votes_count = 0;
@@ -34,5 +36,5 @@ namespace golos { namespace api {
 } } // golos::api
 
 FC_REFLECT_DERIVED( (golos::api::discussion), ((golos::api::comment_api_object)),
-        (url)(pending_payout_value)(total_pending_payout_value)(active_votes)(active_votes_count)(replies)
+        (url)(pending_payout_value)(pending_payout_gests_value)(pending_payout_golos_value)(total_pending_payout_value)(active_votes)(active_votes_count)(replies)
         (author_reputation)(promoted)(body_length)(reblogged_by)(first_reblogged_by)(first_reblogged_on))

zxcat/golos

libraries/api/include/golos/api/discussion.hpp
@@ -16,8 +16,18 @@ namespace golos { namespace api {
         }
 
         string url; /// /category/@rootauthor/root_permlink#author/permlink
+
+        asset pending_author_payout_value = asset(0, SBD_SYMBOL);
+        asset pending_author_payout_gbg_value = asset(0, SBD_SYMBOL);
+        asset pending_author_payout_gests_value = asset(0, VESTS_SYMBOL);
+        asset pending_author_payout_golos_value = asset(0, STEEM_SYMBOL);
+        asset pending_benefactor_payout_value = asset(0, SBD_SYMBOL);
+        asset pending_benefactor_payout_gests_value = asset(0, VESTS_SYMBOL);
+        asset pending_curator_payout_value = asset(0, SBD_SYMBOL);
+        asset pending_curator_payout_gests_value = asset(0, VESTS_SYMBOL);
         asset pending_payout_value = asset(0, SBD_SYMBOL); ///< sbd
         asset total_pending_payout_value = asset(0, SBD_SYMBOL); ///< sbd including replies
+
         std::vector<vote_state> active_votes;
         uint32_t active_votes_count = 0;
         std::vector<string> replies; ///< author/slug mapping
@@ -34,5 +44,9 @@ namespace golos { namespace api {
 } } // golos::api
 
 FC_REFLECT_DERIVED( (golos::api::discussion), ((golos::api::comment_api_object)),
-        (url)(pending_payout_value)(total_pending_payout_value)(active_votes)(active_votes_count)(replies)
+        (url)(pending_author_payout_value)(pending_author_payout_gbg_value)
+        (pending_author_payout_gests_value)(pending_author_payout_golos_value)
+        (pending_benefactor_payout_value)(pending_benefactor_payout_gests_value)
+        (pending_curator_payout_value)(pending_curator_payout_gests_value)
+        (pending_payout_value)(total_pending_payout_value)(active_votes)(active_votes_count)(replies)
         (author_reputation)(promoted)(body_length)(reblogged_by)(first_reblogged_by)(first_reblogged_on))