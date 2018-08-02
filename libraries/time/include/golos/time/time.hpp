1f7f4f23819518a54ecad82e4a2fd35fa6c4a351
Semen Medvedev <kiathai@gmail.com>
2018-08-02 12:40:02 +0700

    Eliminate the inbuilt NTP-client #753

--- libraries/time/include/golos/time/time.hpp
+++ libraries/time/include/golos/time/time.hpp
@@ -10,16 +10,7 @@ namespace golos {
         typedef fc::signal<void()> time_discontinuity_signal_type;
         extern time_discontinuity_signal_type time_discontinuity_signal;
 
-        fc::optional<fc::time_point> ntp_time();
-
         fc::time_point now();
-
-        fc::time_point nonblocking_now(); // identical to now() but guaranteed not to block
-        void update_ntp_time();
-
-        fc::microseconds ntp_error();
-
-        void shutdown_ntp_time();
 
         void start_simulated_time(const fc::time_point sim_time);
 
